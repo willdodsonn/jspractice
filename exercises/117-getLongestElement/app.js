191a52bdb28235257f994a7dcf6ff7cbd795bfdd
willdodsonn <sodlax38@gmail.com>
2023-04-25 01:47:44 +0000

    some done

--- exercises/117-getLongestElement/app.js
+++ exercises/117-getLongestElement/app.js
@@ -1,6 +1,16 @@
 function getLongestElement(arr) {
     // your code here
+    if(arr.length === 0){
+        return '';
+    }
     
+    let bigWord = arr[0];
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i].length > bigWord.length){
+            bigWord = arr[i];
+        }
+    }
+    return bigWord;
 }
 let output = getLongestElement(['one', 'two', 'three']);
 console.log(output); // --> 'three'
