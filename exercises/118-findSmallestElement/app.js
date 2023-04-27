713bc756a61de812ea8a8be2c3cb93851fe2ced6
willdodsonn <sodlax38@gmail.com>
2023-04-27 19:30:33 +0000

    yes

--- exercises/118-findSmallestElement/app.js
+++ exercises/118-findSmallestElement/app.js
@@ -1,7 +1,27 @@
+//does the same thing!!
+
 function findSmallestElement(arr) {
     // your code here
-
+    if(arr.length === 0){
+        return 0;
+    }
+    let smallest = arr[0];
+    let small = arr.filter((num)=>num < smallest);
+    return small;
 }
 
 let output = findSmallestElement([4, 1, 9, 10]);
 console.log(output); // --> 1
+
+// function findSmallestElement(arr) {
+//     // your code here
+//     let aux = 9999999999;
+//     if (arr.length < 1) return 0;
+//     for (let e of arr) {
+//       if (e < aux) aux = e;
+//     }
+//     return aux;
+//   }
+  
+//   let output = findSmallestElement([4, 1, 9, 10]);
+//   console.log(output);
--- exercises/119-findShortestElement/app.js
+++ exercises/119-findShortestElement/app.js
@@ -1,6 +1,13 @@
 function findShortestElement(arr) {
     // your code here
-
+    if (arr.length === 0) {
+        return '';
+    }
+    
+    let small = arr.reduce((acc, short) => {
+        return acc.length < short.length ? acc : short
+    }, arr[0])
+    return small;
 }
 
 let output = findShortestElement(['a', 'two', 'three']);
