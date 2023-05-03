eec00ab1354ec655f582b50a925a15cadaf68a11
willdodsonn <sodlax38@gmail.com>
2023-05-03 19:38:25 +0000

    new things

--- exercises/128-findShortestWordAmongMixedElements/app.js
+++ exercises/128-findShortestWordAmongMixedElements/app.js
@@ -1,5 +1,17 @@
 function findShortestWordAmongMixedElements(arr) {
     // your code here
+    if (arr.length === 0 || !typeof arr == "string") {
+        return '';
+    }
+    let shortest = '';
+    for (let i = 0; i < arr.length; i++) {
+        if (typeof arr[i] ==='string') {
+            if (shortest === '' || arr[i].length < shortest.length) {
+                shortest = arr[i];
+            }
+        }
+    }
+    return shortest;
 }
 let output = findShortestWordAmongMixedElements([4, 'two', 2, 'three']);
 console.log(output); // --> 'two'
